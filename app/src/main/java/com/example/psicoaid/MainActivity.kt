aafa51655797b77bfa40a6f53b23adccde0b3485
Urzlock <117121639+Urzlock@users.noreply.github.com>
2022-11-15 11:46:59 -0600

    Inicio de diseño y codigo del Registro de Psicologo

--- app/src/main/java/com/example/psicoaid/MainActivity.kt
+++ app/src/main/java/com/example/psicoaid/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.psicoaid
 //Este Activity esta planteado para ser el Inicio de Sesión
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRegistrarPsic : Button= findViewById(R.id.btnRegistroPsic)
+        btnRegistrarPsic.setOnClickListener {
+            startActivity(Intent(this,registroPsicologo::class.java))
+        }
     }
 }
