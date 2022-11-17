210498cd37633e6ab63319c123ea6484b7f76a17
Urzlock <117121639+Urzlock@users.noreply.github.com>
2022-11-17 12:47:31 -0600

    Registro de Psicologo COMPLETADO (MySQL Funcionando)

--- app/src/main/java/com/example/psicoaid/registroPsicologo.kt
+++ app/src/main/java/com/example/psicoaid/registroPsicologo.kt
@@ -5,15 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputFilter
 import android.text.InputFilter.LengthFilter
-import android.text.Spanned
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
-import java.util.regex.Pattern
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+
+
+import org.json.JSONException
+import org.json.JSONObject
 
 class registroPsicologo : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -21,49 +28,69 @@ class registroPsicologo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_psicologo)
-        val adapter1 = ArrayAdapter.createFromResource(this,R.array.especialidades,android.R.layout.simple_spinner_item).also { arrayAdapter ->arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)  }
-        val adapter2 = ArrayAdapter.createFromResource(this,R.array.sexo,android.R.layout.simple_spinner_item).also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)  }
-        var textViewtxt: TextView = findViewById(R.id.txtEjemplo)
-        val  textNombre : EditText = findViewById(R.id.textNombre)
+        val adapter1 = ArrayAdapter.createFromResource(
+            this,
+            R.array.especialidades,
+            android.R.layout.simple_spinner_item
+        )
+            .also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item) }
+        val adapter2 = ArrayAdapter.createFromResource(
+            this,
+            R.array.sexo,
+            android.R.layout.simple_spinner_item
+        )
+            .also { arrayAdapter -> arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item) }
+        val textView= findViewById<TextView>(R.id.textView)
+        val textNombre: EditText = findViewById(R.id.textNombre)
         val textApellidos: EditText = findViewById(R.id.textApellidos)
-        val textEdad : EditText= findViewById(R.id.textEdad)
+        val textEdad: EditText = findViewById(R.id.textEdad)
         textEdad.filters = arrayOf<InputFilter>(LengthFilter(2))
-        val textSexo : Spinner = findViewById(R.id.textSexo)
-        val textEspecialidad= findViewById<Spinner>(R.id.textEspecialidad)
+        val textSexo: Spinner = findViewById(R.id.textSexo)
+        val textEspecialidad = findViewById<Spinner>(R.id.textEspecialidad)
         val textNombreUser: EditText = findViewById(R.id.textNombreUser)
-        val textTelefono : EditText = findViewById(R.id.textTelefono)
-        val textCorreo : EditText = findViewById(R.id.textCorreo)
-        val textPassword : EditText = findViewById(R.id.textPassword)
+        val textTelefono: EditText = findViewById(R.id.textTelefono)
+        val textCorreo: EditText = findViewById(R.id.textCorreo)
+        val textPassword: EditText = findViewById(R.id.textPassword)
         val btnRegisterPsic: Button = findViewById(R.id.btnRegisterPsic)
-        textEspecialidad.adapter= adapter1
-        textSexo.adapter=adapter2
+        textEspecialidad.adapter = adapter1
+        textSexo.adapter = adapter2
 
-        var nombre:String
-        var apellidos:String
-        var edad:String
-        var sexo:String
-        var especialidad:String
-        var nombreUser:String
-        var telefono:String
-        var correo:String
-        var password:String
+        var nombre: String
+        var apellidos: String
+        var edad: String
+        var sexo: String
+        var especialidad: String
+        var nombreUser: String
+        var telefono: String
+        var correo: String
+        var password: String
 
 
         btnRegisterPsic.setOnClickListener {
-           if(textNombre.text.isNotEmpty() && textApellidos.text.isNotEmpty() && textEdad.text.isNotEmpty() && textNombreUser.text.isNotEmpty() && textTelefono.text.isNotEmpty() && textCorreo.text.isNotEmpty()  && textPassword.text.isNotEmpty()){
-                nombre=textNombre.text.toString()
-               apellidos=textApellidos.text.toString()
-               edad=textEdad.text.toString()
-               sexo=textSexo.selectedItem.toString()
-               especialidad=textEspecialidad.selectedItem.toString()
-               nombreUser=textNombreUser.text.toString()
-               telefono=textTelefono.text.toString()
-               correo=textCorreo.text.toString()
-               password=textPassword.text.toString()
-               if(validarPassword(password)==true){
-                    Toast.makeText(this,"Contraseña valida",Toast.LENGTH_LONG).show()
-               }
-               else{
+            if (textNombre.text.isNotEmpty() && textApellidos.text.isNotEmpty() && textEdad.text.isNotEmpty() && textNombreUser.text.isNotEmpty() && textTelefono.text.isNotEmpty() && textCorreo.text.isNotEmpty() && textPassword.text.isNotEmpty()) {
+                nombre = textNombre.text.toString()
+                apellidos = textApellidos.text.toString()
+                edad = textEdad.text.toString()
+                sexo = textSexo.selectedItem.toString()
+                especialidad = textEspecialidad.selectedItem.toString()
+                nombreUser = textNombreUser.text.toString()
+                telefono = textTelefono.text.toString()
+                correo = textCorreo.text.toString()
+                password = textPassword.text.toString()
+                if (validarPassword(password) == true) {
+                    Toast.makeText(this, "Contraseña valida", Toast.LENGTH_LONG).show()
+                    val url="http://192.168.1.116:80/PsicoAid/ingreso.php?nombre=${nombre}&apellidos=${apellidos}&edad=${edad}&sexo=${sexo}"
+                    val servicio = Volley.newRequestQueue(this)
+                    val stringRequest = StringRequest(Request.Method.GET, url,
+                        Response.Listener<String> { response ->
+                            // Display the first 500 characters of the response string.
+
+                        },
+                        Response.ErrorListener { textView.text = "That didn't work!" })
+                    servicio.add(stringRequest)
+
+                }
+                else{
                    Toast.makeText(this,"Contraseña invalida,Ingrese 1 Numero, 1 Mayuscula y Minuscula, 1 Caracter Especial y sin espacios en 6 o mas caracteres",Toast.LENGTH_LONG).show()
                }
                Toast.makeText(this,"${sexo}, ${especialidad}",Toast.LENGTH_LONG).show()
@@ -83,4 +110,5 @@ class registroPsicologo : AppCompatActivity() {
             return passwordMatcher.find(text) != null
         } ?: return false
     }
+
 }
